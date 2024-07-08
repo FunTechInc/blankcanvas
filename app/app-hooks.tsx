e3344fd6d53c02387dbc422738d7318eccca5ca8
takuma-hmng8 <t.hashimoto@funtech.inc>
2024-07-08 12:45:41 +0900

    fix:cssvariables

--- app/app-hooks.tsx
+++ app/app-hooks.tsx
@@ -4,12 +4,14 @@ import { useDeviceDetector, useStarter } from "@funtech-inc/spice";
 
 export const AppHooks = () => {
    useStarter();
-   const { isMobile } = useDeviceDetector();
+   const { testing } = useDeviceDetector((ua) => {
+      return /\bLine\b/.test(ua);
+   });
    return (
       <style jsx global>{`
          :root {
-            --fixed-svh: ${isMobile ? `${window.innerHeight / 100}px` : "1svh"};
-            --fixed-lvh: ${isMobile
+            --stable-svh: ${testing ? `${window.innerHeight / 100}px` : "1svh"};
+            --stable-lvh: ${testing
                ? `${window.screen.height / 100}px`
                : "1lvh"};
          }
